9b85fdd93facfd25a7ead436721a36f62e26694f
Anoob Bava <anoob.bava@gmail.com>
2018-12-04 21:16:40 +0530

    added message in carousel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,11 +2,14 @@
   <v-container grid-list-md text-xs-center>
     <v-layout row wrap>
       <v-flex xs12>
-        <v-card dark color="primary">
+        <v-card dark color='primary'>
           <v-carousel>
             <v-carousel-item v-for="(item, index) in imageItems" 
                 :key="index"
                 :src="item.imageUrl">
+                <div class='title'>
+                    {{item.name}}
+                </div>
             </v-carousel-item>
           </v-carousel>
         </v-card>
@@ -20,11 +23,21 @@
         data () {
             return {
                 imageItems: [
-                    {imageUrl: 'https://libreshot.com/wp-content/uploads/2013/11/mongolian-sacred-place-ovoo.jpg' , name: 'meetup1' }, 
-                    {imageUrl: 'https://www.goodfreephotos.com/albums/united-states/wisconsin/hoffman-hills-state-recreation-area/wisconsin-hoffman-hills-state-recreation-area-picnic-place.jpg', name: 'meetup2' }, 
-                    {imageUrl: 'https://www.mediastorehouse.com/image.php?large=13923395', name: 'meetup 3'} 
+                    {imageUrl: 'https://libreshot.com/wp-content/uploads/2013/11/mongolian-sacred-place-ovoo.jpg' , name: 'meetup in KPY' }, 
+                    {imageUrl: 'https://www.goodfreephotos.com/albums/united-states/wisconsin/hoffman-hills-state-recreation-area/wisconsin-hoffman-hills-state-recreation-area-picnic-place.jpg', name: 'Mettup in India' }, 
+                    {imageUrl: 'https://www.mediastorehouse.com/image.php?large=13923395', name: 'Meetup in Australia'} 
                 ]
             }
         }
     }    
 </script>
+
+<style scoped>
+    .title{
+        position: absolute;
+        padding: 30px;
+        bottom: 50px;
+        font-size: 2em;
+    }
+    
+</style>
